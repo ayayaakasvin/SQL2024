f28f90155e6ab5c4a1506f6aebec9f10c48ee72d
Nurik <ayayaakasvin@gmail.com>
2024-11-06 15:56:44 +0500

    lab6 add

--- Lab6/lab6.sql
+++ Lab6/lab6.sql
@@ -1,3 +1,32 @@
+-- 1
+CREATE DATABASE lab6;
+
+-- 2
+CREATE TABLE locations(
+    location_id SERIAL PRIMARY KEY,
+    street_address VARCHAR(25),
+    postal_code VARCHAR(12),
+    city VARCHAR(30),
+    state_province VARCHAR(12)
+);
+
+CREATE TABLE departments(
+    department_id SERIAL PRIMARY KEY,
+    department_name VARCHAR(50) UNIQUE,
+    budget INTEGER,
+    location_id INTEGER REFERENCES locations
+);
+
+CREATE TABLE employees(
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(20),
+    salary INTEGER,
+    department_id INTEGER REFERENCES departments
+);
+
 -- 3
 SELECT employees.first_name, employees.last_name, employees.department_id, departments.department_name
 FROM employees
